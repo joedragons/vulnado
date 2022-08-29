2c8a8871e5a619633215b6ca06a85fe046ee9604
Joe Tom <jtom@sonatype.com>
2022-08-29 16:00:23 -0400

    multiattempt

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -15,4 +15,9 @@ public class CowController {
         return Cowsay.run(input);
     }
     SSLContext sc = SSLContext.getInstance("SSL");
+    SSLContext sc2 = SSLContext.getInstance("SSL");
+    SSLContext sc3 = SSLContext.getInstance("SSL");
+    SSLContext sc4 = SSLContext.getInstance("SSL");
+    SSLContext sc5 = SSLContext.getInstance("SSL");
+    SSLContext sc6 = SSLContext.getInstance("SSL");
 }
